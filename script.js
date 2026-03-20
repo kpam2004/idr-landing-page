5fad7c3bd04eb6ba06af04e9b4d06be90614b681
Krishna Pamnani <154067720+kpam2004@users.noreply.github.com>
2026-03-20 15:45:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({
+        behavior: 'smooth'
+      });
+  });
+});
